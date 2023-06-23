3027eb2000b888b663e87c39edf157520c590b65
ksk <kskw08420@gmail.com>
2023-06-23 18:51:18 +0900

    #40 - 게시글 관리 페이지 구현: 컨트롤러, 서비스, 뷰

--- src/main/java/com/fastcampus/projectboardadmin/controller/ArticleManagementController.java
+++ src/main/java/com/fastcampus/projectboardadmin/controller/ArticleManagementController.java
@@ -1,24 +1,49 @@
 package com.fastcampus.projectboardadmin.controller;
 
+import com.fastcampus.projectboardadmin.dto.response.ArticleResponse;
+import com.fastcampus.projectboardadmin.service.ArticleManagementService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/articles")
 @Controller
 public class ArticleManagementController {
 
+    private final ArticleManagementService articleManagementService;
+
     @GetMapping
-    public String articles(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            Model model
-    ) {
+    public String articles(Model model) {
+        model.addAttribute(
+                "articles",
+                articleManagementService.getArticles().stream().map(ArticleResponse::withoutContent).toList()
+        );
+
         model.addAttribute("context", "/management");
         model.addAttribute("uri", "/management/articles");
         return "management/articles";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/{articleId}")
+    public ArticleResponse article(@PathVariable Long articleId) {
+        return ArticleResponse.withContent(articleManagementService.getArticle(articleId));
+    }
+
+    @PostMapping("/{articleId}")
+    public String deleteArticle(@PathVariable Long articleId) {
+        articleManagementService.deleteArticle(articleId);
+
+        return "redirect:/management/articles";
     }
 }
