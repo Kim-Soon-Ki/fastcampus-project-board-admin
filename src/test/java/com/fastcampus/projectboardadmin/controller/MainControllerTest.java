db9847fc668a8b7b08466425b3963ddcf3df753c
ksk <kskw08420@gmail.com>
2023-06-28 20:28:01 +0900

    #63 - Actuator 의 metric 수집을 활용한 방문 숫자 집계 서비스 및 컨트롤러 어드바이스 구현
    전체 컨트롤러에 적용되는 설정이므로 컨트롤러 어드바이스로 접근
    이에 모든 컨트롤러 테스트가 영향 받으므로 해당 영향을 표현하는
    전역 테스트 설정을 작성하고, 컨트롤러 테스트들에 추가

--- src/test/java/com/fastcampus/projectboardadmin/controller/MainControllerTest.java
+++ src/test/java/com/fastcampus/projectboardadmin/controller/MainControllerTest.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboardadmin.controller;
 
+import com.fastcampus.projectboardadmin.config.GlobalControllerConfig;
 import com.fastcampus.projectboardadmin.config.SecurityConfig;
 import com.fastcampus.projectboardadmin.config.TestSecurityConfig;
 import org.junit.jupiter.api.DisplayName;
@@ -17,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @DisplayName("View 루트 컨트롤러")
-@Import(TestSecurityConfig.class)
+@Import({TestSecurityConfig.class, GlobalControllerConfig.class})
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
